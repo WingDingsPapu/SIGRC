f975f22cf026d2af1ae66ea5c3cca99fd02667a0
Josue Daniel Razuri Paredes <josuejoestar90@gmail.com>
2026-06-10 18:31:30 -0500

    Se agrega clase Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+
+    private String codigo;
+    private String nombre;
+
+    public Cliente(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Código: " + codigo);
+        System.out.println("Nombre: " + nombre);
+    }
+}
