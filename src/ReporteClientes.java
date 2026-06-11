ceadd957cc2fe565f1ba9445f44e38bb83eb9134
Josue Daniel Razuri Paredes <josuejoestar90@gmail.com>
2026-06-10 21:01:04 -0500

    Se agrega validacion de lista nula

--- src/ReporteClientes.java
+++ src/ReporteClientes.java
@@ -4,6 +4,11 @@ public class ReporteClientes {
 
     public void generarReporte(ArrayList<String> clientes) {
 
+        if(clientes == null){
+            System.out.println("No existen clientes.");
+            return;
+        }
+
         System.out.println("==== REPORTE DE CLIENTES ====");
 
         for(String cliente : clientes){
