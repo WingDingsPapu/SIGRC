d2f130473197d8845fc26f0dc4435a516c5f8ab6
Josue Daniel Razuri Paredes <josuejoestar90@gmail.com>
2026-06-19 15:02:49 -0500

    Se agregan setters y metodo toString

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,15 +1,50 @@
 public class Cliente {
 
     private String codigo;
-    private String nombreCliente;
+    private String nombre;
+    private String email;
 
-    public Cliente(String codigo, String nombreCliente) {
+    public Cliente(String codigo, String nombre) {
         this.codigo = codigo;
-        this.nombreCliente = nombreCliente;
+        this.nombre = nombre;
     }
 
-    public void mostrarDatos() {
-        System.out.println("Código: " + codigo);
-        System.out.println("Nombre: " + nombreCliente);
+    public Cliente(String codigo,
+                   String nombre,
+                   String email) {
+
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+
+        return codigo
+                + " - "
+                + nombre
+                + " - "
+                + email;
     }
 }
